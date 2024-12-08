320a17de3c3c0a832492c1317956e1b12f693bbf
victortejeda <victorfernandotejeda@gmail.com>
2024-12-08 12:23:58 -0400

    Initial Commit

---
+++ prueva/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prueva
+//
+//  Created by Victor Tejeda on 8/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prueva/pruevaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruevaApp.swift
+//  prueva
+//
+//  Created by Victor Tejeda on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct pruevaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
